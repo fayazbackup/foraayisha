32989f23443108f614d38fe8587e49e957d589be
fayazbackup <mainpesacc1@gmail.com>
2025-08-10 20:08:02 +0530

    blur js

--- base.js
+++ base.js
@@ -1,3 +1,13 @@
-document.getElementById("messageBox").addEventListener("click", function() {
-    window.location.href = "index1.html";
+const msgBox = document.querySelector('.message-box');
+const body = document.querySelector('body');
+
+msgBox.addEventListener('mouseenter', () => {
+    body.classList.add('blur-bg');
+});
+msgBox.addEventListener('mouseleave', () => {
+    body.classList.remove('blur-bg');
+});
+
+msgBox.addEventListener('click', () => {
+    window.location.href = "index.html"; // Change to your target page
 });
